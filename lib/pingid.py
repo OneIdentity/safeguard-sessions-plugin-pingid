cd3192d2d20dc3698ec55be16efa9ac6fc763fe2
darazsdome <darazs.dome@quest.com>
2020-07-07 19:53:22 +0200

    pingid: client is now a subclass of the PluginSDK provided MFAClient class
    Signed-off-by: Dome Darazs <dome.darazs@oneidentity.com>

--- lib/pingid.py
+++ lib/pingid.py
@@ -1,35 +1,48 @@
 #!/usr/bin/env pluginwrapper3
 
-import logging
 import json
-from datetime import datetime
-from copy import deepcopy
+import logging
 from base64 import urlsafe_b64decode
+from copy import deepcopy
+from datetime import datetime
 
 import jwt
 import requests
 
+from safeguard.sessions.plugin import AAResponse
+from safeguard.sessions.plugin.mfa_client import (
+    MFAClient,
+    MFAAuthenticationFailure,
+    MFACommunicationError,
+)
+
 logger = logging.getLogger(__name__)
 
 
-class Client:
+class Client(MFAClient):
     API_VERSION = 4.9
     JWS_ALG = "HS256"
 
     @classmethod
-    def from_config(cls, configuration):
+    def from_config(cls, configuration, device_id, push_display_text):
         url = configuration.get("pingid", "idp_url", required=True)
         alias = configuration.get("pingid", "org_alias", required=True)
         token = configuration.get("pingid", "token", required=True)
         key = urlsafe_b64decode(configuration.get("pingid", "base64_key", required=True))
+        logo_url = configuration.get("pingid", "logo_url", default=None)
 
-        return cls(url, alias, token, key)
+        return cls(url, alias, token, key, logo_url, device_id, push_display_text)
 
-    def __init__(self, url, alias, token, key):
+    def __init__(self, url, alias, token, key, logo_url, device_id, push_display_text):
+        super().__init__("SPS PingID Plugin")
+
         self.base_url = url
         self.alias = alias
         self.token = token
         self.key = key
+        self.logo_url = logo_url
+        self.device_id = device_id
+        self.push_display_text = push_display_text
 
         self.jwt_headers = {
             "alg": self.JWS_ALG,
@@ -39,6 +52,91 @@ class Client:
         self.http_headers = {
             "Content-Type": "application/json"
         }
+        logger.info("Client initialized.")
+
+
+    def otp_authenticate(self, mfa_identity, otp):
+        response = self._auth_online(mfa_identity, request_otp=True)
+        session_id = self._extract_info(response, "sessionId")
+        response = self._auth_offline(mfa_identity, otp, session_id)
+
+        return self._get_verdict(response)
+
+    def push_authenticate(self, mfa_identity):
+        response = self._auth_online(
+            mfa_identity, sp_name=self.push_display_text, sp_logo=self.logo_url,
+        )
+        return self._get_verdict(response)
+
+    def auth_start(self, username, device_id=None):
+        params = {}
+        params.update({"deviceId": device_id} if device_id else {})
+        return self._call_api("/rest/4/startauthentication/do", username, params)
+
+    def _auth_online(self, username, request_otp=False, sp_name=None, sp_logo=None):
+        params = {}
+        params.update({"deviceId": self.device_id} if self.device_id else {})
+        params.update({"authType": "OTP"} if request_otp else {})
+
+        params.update({"formParameters": {}})
+        params["formParameters"].update({"sp_name": sp_name} if sp_name else {})
+        params["formParameters"].update({"sp_logo": sp_logo} if sp_logo else {})
+
+        return self._call_api("/rest/4/authonline/do", username, params)
+
+    def _auth_offline(self, username, otp, session_id):
+        params = {}
+        params.update({"otp": otp})
+        params.update({"sessionId": session_id})
+        return self._call_api("/rest/4/authoffline/do", username, params)
+
+    def _call_api(self, path, username, params):
+        request = {
+            "reqHeader": self._get_request_headers(),
+            "reqBody": self._get_request_body(username, params)
+        }
+        encoded_request = self._jwt_encode(request)
+        url = self.base_url + path
+
+        logger.debug("Running API call; url=%s; headers=%s; request=%s",
+                     url, self.http_headers, self._filter_request(request))
+        try:
+            response = requests.post(url, encoded_request, headers=self.http_headers)
+            content = self._extract_json(response.content)
+            logger.debug(
+                "Response received; status_code=%i; reason=%s; response=%s",
+                response.status_code,
+                response.reason,
+                content,
+            )
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            raise MFAAuthenticationFailure(self._get_reason(content))
+        except requests.exceptions.RequestException as e:
+            raise MFACommunicationError(e) from e
+
+        return content
+
+    def _extract_info(self, response, key):
+        return response.get("responseBody", {}).get(key)
+
+    def _get_reason(self, response):
+        # See this link for more info:
+        # https://apidocs.pingidentity.com/pingid-api/guide/pingid-api/pid_c_PingIDapiErrorCodes/
+        response_id = self._extract_info(response, "errorId")
+        if response_id in range(2500, 2600) or response_id in range(30000, 30015):
+            return self._extract_info(response, "errorMsg")
+
+    def _get_verdict(self, response):
+        response_id = self._extract_info(response, "errorId")
+        response_msg = self._extract_info(response, "errorMsg")
+
+        if response_id == 200:
+            logger.info("Authentication successful")
+            return AAResponse.accept(reason="Authentication successful")
+
+        logger.info("Authentication failure; id=%i, msg=%s", response_id, response_msg)
+        return AAResponse.deny(reason=self._get_reason(response))
 
     def _get_timestamp(self):
         return datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]  # Convert microsec to milisec
@@ -79,67 +177,6 @@ class Client:
             return json.loads(data)
         except ValueError:
             raise ResponseError("Failed to JWT/JSON decode response")
-
-    def _call_api(self, path, username, params):
-        request = {
-            "reqHeader": self._get_request_headers(),
-            "reqBody": self._get_request_body(username, params)
-        }
-        encoded_request = self._jwt_encode(request)
-        url = self.base_url + path
-
-        logger.debug("Running API call; url=%s; headers=%s; request=%s",
-                     url, self.http_headers, self._filter_request(request))
-        try:
-            response = requests.post(url, encoded_request, headers=self.http_headers)
-            content = self._extract_json(response.content)
-            logger.debug("Response received; status_code=%i; reason=%s; response=%s",
-                         response.status_code, response.reason, content)
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            raise HTTPError(e, content=content)
-        except requests.exceptions.RequestException as e:
-            raise CommunicationError(e) from e
-
-        return content
-
-    def auth_start(self, username, device_id=None):
-        params = {}
-        params.update({"deviceId": device_id} if device_id else {})
-        return self._call_api("/rest/4/startauthentication/do", username, params)
-
-    def auth_online(self, username, device_id=None, request_otp=False, sp_name=None, sp_logo=None):
-        params = {}
-        params.update({"deviceId": device_id} if device_id else {})
-        params.update({"authType": "OTP"} if request_otp else {})
-
-        params.update({"formParameters": {}})
-        params["formParameters"].update({"sp_name": sp_name} if sp_name else {})
-        params["formParameters"].update({"sp_logo": sp_logo} if sp_logo else {})
-
-        return self._call_api("/rest/4/authonline/do", username, params)
-
-    def auth_offline(self, username, otp, session_id):
-        params = {}
-        params.update({"otp": otp})
-        params.update({"sessionId": session_id})
-        return self._call_api("/rest/4/authoffline/do", username, params)
-
-
-class ClientError(Exception):
-    """Base exception class for all PingID Client library related exceptions.
-       It should be subclassed and not raised directly."""
-    pass
-
-
-class CommunicationError(ClientError, requests.exceptions.RequestException):
-    pass
-
-
-class HTTPError(ClientError):
-    def __init__(self, *args, **kwargs):
-        self.content = kwargs.pop("content", None)
-        super().__init__(*args, **kwargs)
 
 
 class ResponseError(Exception):
--- lib/plugin.py
+++ lib/plugin.py
@@ -2,20 +2,20 @@
 
 
 from safeguard.sessions.plugin import AAPlugin, AAResponse
+from safeguard.sessions.plugin.exceptions import PluginSDKValueError
 from safeguard.sessions.plugin.plugin_base import cookie_property
 
-from .pingid import Client, ResponseError, HTTPError, CommunicationError
+from .pingid import Client, ResponseError
 
 
 class Plugin(AAPlugin):
     def __init__(self, configuration, client=None):
         super().__init__(configuration)
 
-        self._logo_url = self.plugin_configuration.get("pingid", "logo_url", default=None)
         self._selection = self.plugin_configuration.getboolean("pingid", "enable_device_selection", default=True)
         self._stacktrace = self.plugin_configuration.getboolean("logging", "enable_stacktrace", default=False)
 
-        self._client = client or Client.from_config(self.plugin_configuration)
+        self._client = client
 
     @cookie_property
     def _device_index_map(self):
@@ -23,8 +23,29 @@ class Plugin(AAPlugin):
 
     @property
     def _push_display_text(self):
-        username = self.connection.gateway_username or self.connection.server_username
-        return "{c.protocol} from {c.client_ip} by {username}".format(c=self.connection, username=username)
+        return "{c.protocol} from {c.client_ip} by {username}".format(c=self.connection, username=self.username)
+
+    @property
+    def username(self):
+        return self.connection.gateway_username or self.connection.server_username
+
+    def do_authenticate(self):
+        reason = None
+        self._client = self._client or Client.from_config(
+            self.plugin_configuration, self._get_device_id(), self._push_display_text
+        )
+        try:
+            return self._dispatch()
+        except InvalidDeviceSelection as e:
+            self.logger.info("Invalid device selected: %s", e)
+            reason = "Invalid device selected."
+        except ResponseError as e:
+            self.logger.error("Invalid response: %s", e, exc_info=self._stacktrace)
+            reason = "Invalid response from PingID API."
+        except Exception as e:
+            self.logger.error("Unexpected error: %s: %s", type(e), e, exc_info=self._stacktrace)
+
+        return AAResponse.deny(reason=reason)
 
     def _get_device_id(self):
         device_index = self._get_device_index()
@@ -32,7 +53,7 @@ class Plugin(AAPlugin):
             return self._device_index_map[device_index]
 
         if device_index is not None:
-            raise InvalidDeviceSelection("selection={}".format(device_index))
+            raise InvalidDeviceSelection(message="selection={}".format(device_index))
 
     def _get_device_index(self):
         return self.connection.key_value_pairs.get("selected_device")
@@ -59,47 +80,13 @@ class Plugin(AAPlugin):
 
         message += "Please select a device: "
 
-        self.logger.debug("Prompting user to select a device; device_ids=%s",
-                          self._device_index_map)
+        self.logger.debug("Prompting user to select a device; device_ids=%s", self._device_index_map)
         return AAResponse.need_info(message, "selected_device")
 
     def _get_auth_info(self):
         self.connection.key_value_pairs.pop("otp")
         self.logger.debug("Prompting for MFA input")
         return self._ask_mfa_password()
-
-    def _extract_info(self, response, key):
-        return response.get("responseBody", {}).get(key)
-
-    def _get_reason(self, response):
-        # See this link for more info:
-        # https://apidocs.pingidentity.com/pingid-api/guide/pingid-api/pid_c_PingIDapiErrorCodes/
-        response_id = self._extract_info(response, "errorId")
-        if response_id in range(2500, 2600) or response_id in range(30000, 30015):
-            return self._extract_info(response, "errorMsg")
-
-    def _get_verdict(self, response):
-        response_id = self._extract_info(response, "errorId")
-        response_msg = self._extract_info(response, "errorMsg")
-
-        if response_id == 200:
-            self.logger.info("Authentication successful")
-            return AAResponse.accept(reason="Authentication successful")
-
-        self.logger.info("Authentication failure; id=%i, msg=%s", response_id, response_msg)
-        return AAResponse.deny(reason=self._get_reason(response))
-
-    def _otp_authenticate(self):
-        response = self._client.auth_online(self.mfa_identity, self._get_device_id(), request_otp=True)
-        session_id = self._extract_info(response, "sessionId")
-        response = self._client.auth_offline(self.mfa_identity, self.mfa_password, session_id)
-
-        return self._get_verdict(response)
-
-    def _push_authenticate(self):
-        response = self._client.auth_online(self.mfa_identity, self._get_device_id(),
-                                            sp_name=self._push_display_text, sp_logo=self._logo_url)
-        return self._get_verdict(response)
 
     def _dispatch(self):
         if self.mfa_password == "!select":
@@ -111,36 +98,8 @@ class Plugin(AAPlugin):
             self.logger.info("Device selection command received")
             return self._select_device()
 
-        if self.mfa_password:
-            self.logger.info("Authenticating with OTP")
-            return self._otp_authenticate()
-
-        self.logger.info("Authenticating with PUSH notification")
-        return self._push_authenticate()
-
-    def do_authenticate(self):
-        reason = None
-        try:
-            return self._dispatch()
-        except InvalidDeviceSelection as e:
-            self.logger.info("Invalid device selected: %s", e)
-        except ResponseError as e:
-            self.logger.error("Invalid response: %s", e, exc_info=self._stacktrace)
-        except HTTPError as e:
-            reason = self._get_reason(e.content)
-            self.logger.info("Authentication failure: %s, msg=%s", e,
-                             self._extract_info(e.content, "errorMsg"))
-        except CommunicationError as e:
-            self.logger.error("Connection failure: %s", e, exc_info=self._stacktrace)
-        except Exception as e:
-            self.logger.error("Unexpected error: %s: %s", type(e), e, exc_info=self._stacktrace)
-
-        return AAResponse.deny(reason=reason)
+        return self._client.execute_authenticate(self.username, self.mfa_identity, self.mfa_password)
 
 
-class PluginError(Exception):
-    pass
-
-
-class InvalidDeviceSelection(PluginError):
+class InvalidDeviceSelection(PluginSDKValueError):
     pass
